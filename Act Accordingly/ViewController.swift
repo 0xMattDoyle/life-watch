0dea726fe513d19a94a8d968b23e170291804837
matt.fyi <hello@matt.fyi>
2015-12-22 10:22:17 +1100

    Initial Commit

---
+++ Act Accordingly/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Act Accordingly
+//
+//  Created by Matt Doyle on 22/12/2015.
+//  Copyright © 2015 llumicode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
