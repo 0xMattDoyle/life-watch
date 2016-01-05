eb07c107723bc0b858bc114533bf0162ac7916fa
matt.fyi <hello@matt.fyi>
2016-01-06 08:42:21 +1100

    Updated logic, moved code to TodayViewController.

--- Act Accordingly/ViewController.swift
+++ Act Accordingly/ViewController.swift
@@ -4,21 +4,39 @@
 //
 //  Created by Matt Doyle on 22/12/2015.
 //  Copyright © 2015 llumicode. All rights reserved.
+//  Matt, go to http://www.colorhunt.co/c/3986 for the pallet.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
     
-    var calculatedLifeDuration = 0
-    var currentHoursRemaining = "50,403"
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
+        // Calcuate totalEstimatedLifetime based on Age, Gender and Country
+        
+        let calendar : NSCalendar = NSCalendar.currentCalendar()
+        let now = NSDate()
+        
+        // Get input from user (currently hardcoded)
+        // let country = "Australia"
+        // let gender = "Male"
+        let birthYear = 1991
+        let birthMonth = 7
+        let birthDay = 16
+        
+        // Calculate age based on DOB
+        let userDOB = NSCalendar.currentCalendar().dateWithEra(1, year: birthYear, month: birthMonth, day: birthDay, hour: 0, minute: 0, second: 0, nanosecond: 0)
+        let ageComponents = calendar.components(.Year, fromDate: userDOB!, toDate: now, options: [])
+        let age = ageComponents.year
+        let usersAgeInDays = age * 365
+        
+        // Setup UserDefaults
         let defaults = NSUserDefaults(suiteName: "group.llumicode.TodayExtensionSharingDefaults")
-        defaults?.setObject("50,403", forKey: "currentHoursRemaining")
+        // Set the string for the Today Extenstion to display
+        defaults?.setObject(usersAgeInDays, forKey: "usersAgeInDays")
+        defaults?.setObject("50,403", forKey: "totalDaysInLifetime")
         defaults?.synchronize()
         
     }
