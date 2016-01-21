8dcbfbf202c913bb259f6bca0697910ffd6cd30c
Matt Doyle <matthew.doyle@amaroo.co>
2016-01-21 11:39:43 +1100

    Storyboard modifications. Removed some FB code.

--- Act Accordingly/ViewController.swift
+++ Act Accordingly/ViewController.swift
@@ -9,8 +9,8 @@
 
 import UIKit
 import Parse
-import FBSDKCoreKit
-import FBSDKLoginKit
+//import FBSDKCoreKit
+//import FBSDKLoginKit
 //import ParseFacebookUtilsV4
 
 class ViewController: UIViewController {
@@ -21,29 +21,10 @@ class ViewController: UIViewController {
     
     // IBOutlets
     @IBOutlet weak var dashMessage: UILabel!
-    @IBOutlet weak var nameHeader: UILabel!
     @IBOutlet weak var lifeExpNumber: UILabel!
     @IBOutlet weak var daysLeftNumber: UILabel!
-    @IBOutlet weak var profilePictureView: UIImageView!
     
     // IBActions
-    @IBAction func logOutDidPress(sender: AnyObject) {
-        
-        NSUserDefaults.standardUserDefaults().removePersistentDomainForName("group.llumicode.TodayExtensionSharingDefaults2")
-        
-        if Reachability.isConnectedToNetwork() == true {
-            
-            PFUser.logOut()
-            
-        } else {
-            
-            PFUser.logOutInBackground()
-            
-        }
-    
-        self.performSegueWithIdentifier("notLoggedIn", sender: self)
-        
-    }
     
     @IBAction func editDidPress(sender: AnyObject) {
         
@@ -74,83 +55,22 @@ class ViewController: UIViewController {
         
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "defaultsChanged:", name: NSUserDefaultsDidChangeNotification, object: nil)
         
-        // Watch for changes in user profile
-        FBSDKProfile.enableUpdatesOnAccessTokenChange(true)
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "onProfileUpdated:", name: FBSDKProfileDidChangeNotification, object: nil)
-        
     }
     
     func defaultsChanged(notification:NSNotification){
         
-        if (FBSDKAccessToken.currentAccessToken() != nil) {
+        let defaults = NSUserDefaults(suiteName: "group.llumicode.TodayExtensionSharingDefaults2")
+        defaults?.synchronize()
+        
+        if defaults?.boolForKey("newUser") == false {
             
-            //User is logged in, so do things with all of the data.
+            //User is not new, so do things with their data.
             updateDashText()
             
         } else {
-            // User is not logged in
+            // User has no data
         }
         
-    }
-    
-    func loadFbImage() {
-        
-        PFUser.currentUser()?.fetchInBackgroundWithBlock({ (user, error) -> Void in
-            
-            if error == nil {
-                
-                let defaults = NSUserDefaults(suiteName: "group.llumicode.TodayExtensionSharingDefaults2")
-                
-                let fbPic = FBSDKProfile.imageURLForPictureMode(FBSDKProfile.currentProfile())
-                let fbPicUrl = fbPic(FBSDKProfilePictureMode.Square, size: CGSizeMake(200, 200))
-                
-                let request: NSURLRequest = NSURLRequest(URL: fbPicUrl)
-                
-                let session = NSURLSession.sharedSession()
-                let task = session.dataTaskWithRequest(request){
-                    (data, response, error) -> Void in
-                    
-                    if (error == nil && data != nil)
-                    {
-                        func saveFbImageToParseAndDefaults()
-                        {
-                            
-                            let file = PFFile(name: "profilePicture.png", data: data!)
-                            user?["profilePicture"] = file
-                            user?.saveInBackground()
-                            
-                            defaults!.setObject(data, forKey: "profilePicture")
-                            defaults?.synchronize()
-                            
-                            if let image = defaults?.objectForKey("profilePicture") as? NSData {
-                                
-                                if let image = UIImage(data: image) {
-                                    
-                                    self.profilePictureView.image = image
-                                    
-                                }
-                            
-                            }
-                            
-                        }
-                        
-                        dispatch_async(dispatch_get_main_queue(), saveFbImageToParseAndDefaults)
-                    }
-                    
-                }
-                
-                task.resume()
-                
-            }
-            
-        })
-        
-    }
-    
-    // Function runs when users profile changes
-    func onProfileUpdated(notification: NSNotification) {
-        
-
     }
     
     func updateDashText () {
@@ -160,16 +80,13 @@ class ViewController: UIViewController {
         
         if defaults?.stringForKey("usersDaysRemaining") != nil && defaults?.stringForKey("totalDaysInLifetime") != nil {
             
-            let firstName = defaults?.stringForKey("firstName")
-            let lastName = defaults?.stringForKey("lastName")
             let totalDaysInLifetime = defaults?.integerForKey("totalDaysInLifetime")
             let lifeExp = totalDaysInLifetime! / 365
             let usersDaysRemaining = defaults?.stringForKey("usersDaysRemaining")
-            self.nameHeader.text = firstName! + " " + lastName!
             self.lifeExpNumber.text = String(lifeExp)
             self.daysLeftNumber.text = usersDaysRemaining
             
-            let textString = String(firstName!) + ", you're expected to live to " + String(lifeExp) + ", that's " + String(usersDaysRemaining!) + " days to do everything you'll ever do. Make them count!"
+            let textString = "You're expected to live to " + String(lifeExp) + ", that's " + String(usersDaysRemaining!) + " days to do everything you'll ever do. Make them count!"
             
             self.dashMessage.text = textString
             
@@ -181,44 +98,20 @@ class ViewController: UIViewController {
         
         let defaults = NSUserDefaults(suiteName: "group.llumicode.TodayExtensionSharingDefaults2")
         defaults?.synchronize()
-        
-        // Check if user logged in
-        if (FBSDKAccessToken.currentAccessToken() != nil) {
             
-            //User is logged in, so do things with all of the data.
-            if defaults?.boolForKey("isNew") == true {
+        //User is logged in, so do things with all of the data.
+        if defaults?.boolForKey("isNew") == true {
                 
-                defaults?.setObject(false, forKey: "isNew")
-                performSegueWithIdentifier("editSegue", sender: self)
+            defaults?.setObject(false, forKey: "isNew")
+            performSegueWithIdentifier("editSegue", sender: self)
                 
-            }
+        } else {
             
-            // Get image from user defaults
-            if let _ = FBSDKProfile.currentProfile() {
-                
-                calulateUsersDaysRemaining()
-                
-                if let image = defaults?.objectForKey("profilePicture") as? NSData {
-                    
-                    if let image = UIImage(data: image) {
-                        
-                        profilePictureView.image = image
-                        
-                    }
-                    
-                } else {
-                    
-                   loadFbImage()
-                    
-                }
-                
-            }
-            
-            updateDashText()
+        updateDashText()
             
         } else {
             // User is not logged in
-            self.performSegueWithIdentifier("notLoggedIn", sender: self)
+            self.performSegueWithIdentifier("newUser", sender: self)
         }
         
     }
